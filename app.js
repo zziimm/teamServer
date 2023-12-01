b6c9899e98fdba486d93b7ba8915248562c4a9c1
zziimm <wlalscjs30@naver.com>
2023-12-01 17:43:38 +0900

    MongoDB 연결 성공

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config();
+const { connect } = require('./database/index');
+
+
 const app = express();
 app.set('port', process.env.PORT || 3002);
+connect();
 
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));  // '/' 경로가 루트면 생략 가능  app.use(express.static(path.join(__dirname, 'public')));
@@ -25,10 +29,24 @@ app.use(session({
   name: 'session-cookie'
 }));
 
+app.get('/', (req, res) => {
+  res.send('하이')
+});
 
 
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
 
-
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port') + '번에서 서버 실행 중');
